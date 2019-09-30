c40878fb17ffb51616ce02eefa6cbe32b6a2bcc8
28619426 <tienhung0182@gmail.comm>
2019-09-30 19:13:52 +0700

    Custom-nav+scroll-effect

--- js/custom.js
+++ js/custom.js
@@ -19,3 +19,15 @@ $(function() {
         autoplayHoverPause: true,
     });
 });
+// Smooth Scroll
+$(function(){
+    $("a.smooth-scroll").click(function(event){
+        event.preventDefault();
+        // Get Href Attribute
+        var section = $(this).attr("href");
+
+        $('html, body').animate({
+            scrollTop: $(section).offset().top
+        }, 1250, "easeInOutExpo");
+    });
+});
