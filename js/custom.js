71c902bd31345850423f5d006b36c846c747b06a
28619426 <tienhung0182@gmail.comm>
2019-09-30 11:05:30 +0700

    finished-product-section

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,22 @@
-// General JS
+// Product (Owl Carousel)
+$(function() {
+    // owl carousel
+    $("#item-list").owlCarousel({
+        items: 4,
+        loop: true,
+        autoplay: true,
+        smartSpeed: 700,
+        autoplayHoverPause: true,
+    });
+});
+// Categories (Owl Carousel)
+$(function() {
+    // owl carousel
+    $("#item-list2").owlCarousel({
+        items: 4,
+        loop: true,
+        autoplay: true,
+        smartSpeed: 700,
+        autoplayHoverPause: true,
+    });
+});
